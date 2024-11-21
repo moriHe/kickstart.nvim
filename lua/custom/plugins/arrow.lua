c3009e3e4b33c863689bf5db18c87ff4dad198e7
Moritz Hettich <mor.hettich@gmail.com>
2024-11-21 23:33:12 +0100

    change mappings arrow

--- lua/custom/plugins/arrow.lua
+++ lua/custom/plugins/arrow.lua
@@ -9,5 +9,17 @@ return {
     show_icons = true,
     leader_key = ';', -- Recommended to be a single key
     buffer_leader_key = 'm', -- Per Buffer Mappings
+    mappings = {
+      edit = 'e',
+      delete_mode = 'D',
+      clear_all_items = 'C',
+      toggle = 't', -- used as save if separate_save_and_remove is true
+      open_vertical = 's',
+      open_horizontal = 'h',
+      quit = 'q',
+      remove = 'd', -- only used if separate_save_and_remove is true
+      next_item = 'n',
+      prev_item = 'p',
+    },
   },
 }
